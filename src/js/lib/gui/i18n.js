0983cfa10ffc034988f751bf44ca60fcd0e85300
Raul Rodrigo <raurodse@gmail.com>
2015-03-10 11:53:52 +0100

    fix bugs

--- src/js/lib/gui/i18n.js
+++ src/js/lib/gui/i18n.js
@@ -1,15 +1,28 @@
-var nodeutil = require('util');
+var nodeutil = require('util'),
+	nodefs = require('fs');
+
 /**
  * Wrapper to Jed library to translate gui.
  * This wrapper extend with dinamically load content
  * @param {[type]} objectdata [description]
  */
 function Translator(){
-	var objectdata = require('./i18n/' + process.env.LANG + '.js');
-	Translator.super_.call(this,objectdata);
+	var translationspath = './i18n/' + process.env.LANG + '.js',
+		objectdata = {},
+		contentfile;
+	if(nodefs.existsSync(translationspath)){
+		try{
+			contentfile = JSON.parse(nodefs.readFileSync(translationspath,{encoding:'utf-8'}));
+		}
+		catch(err){
+			contentfile = {};
+		}
+		$.extend(objectdata,contentfile);
+	}
+	//	Translator.super_.call(this,objectdata);
 }
 nodeutil.inherits(Translator,Jed);
 
 var util = application.util.core.getInstance();
 util.createNameSpace('application.util.i18n');
-application.util.i18n = CBUtil.singleton(Translator());
+application.util.i18n = util.singleton(Translator());
